3b663d557e9c8d4c41a232ac4e526f67295f0936
vukolro <vukolovrodion@gmail.com>
2022-07-08 13:18:43 +0400

    feat: setup project

---
+++ android/app/src/main/kotlin/com/vukolro/quizzler_app_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vukolro.quizzler_app_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
